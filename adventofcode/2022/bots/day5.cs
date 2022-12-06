0406dae1654d23258631036f571766f24e9c92ab
Geoffrey Lefèvre <lefevregeoffrey@gmail.com>
2022-12-07 00:36:56 +0100

    Day 5 + debut Day 6

---
+++ adventofcode/2022/bots/day5.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode._2022.bots
+{
+    public class BotDay5:IBot
+    {
+        int length = 0;
+        int nbrang = 0;
+        Stack<char>[] caisses;
+        Stack<char>[] caissesTmp;
+        public void LoadData()
+        {
+            string path = @"./2022/inputs/day5.txt";
+            var mode = 1;
+            foreach (string line in System.IO.File.ReadLines(path))
+            {
+                if(length==0)
+                {
+                    length = line.Length;
+                    nbrang = (length + 1) / 4;
+                    caisses = new Stack<char>[nbrang];
+                    caissesTmp = new Stack<char>[nbrang];
+                    for (int i = 0; i < nbrang; i++)
+                    {
+                        caissesTmp[i] = new Stack<char>();
+                        caisses[i] = new Stack<char>();
+                    }
+                }
+                if (mode == 1)
+                {
+                    if (line[1] == '1')
+                    {
+                        mode = 2;
+                        for(int i = 0; i < nbrang; i++)
+                        {
+                            while (caissesTmp[i].Count != 0)
+                            {
+                                caisses[i].Push(caissesTmp[i].Pop());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int rang = 0;
+                        for (int i = 1; i < length; i += 4)
+                        {
+                            if (line[i] != ' ')
+                                caissesTmp[rang].Push(line[i]);
+                            rang++;
+                        }
+                    }
+                }
+                else
+                {
+                    if(!string.IsNullOrWhiteSpace(line))
+                    {
+                        var move = line.Split(' ');
+                        var nb = int.Parse(move[1]);
+                        var from = int.Parse(move[3])-1;
+                        var to = int.Parse(move[5])-1;
+                        for(int m=0;m<nb;m++)
+                        {
+                            caisses[to].Push(caisses[from].Pop());
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Excute()
+        {
+            foreach(var c in caisses)
+            {
+                Console.Write(c.Pop());
+            }
+        }
+
+    }
+}
---
+++ adventofcode/2022/bots/day6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode._2022.bots
+{
+    public class BotDay6:IBot
+    {
+        HashSet<char> window = new HashSet<char>(4);
+        int maxtmp = 0;
+        public void LoadData()
+        {
+            string path = @"./2022/inputs/day1.txt";
+            foreach (string line in System.IO.File.ReadLines(path))
+            {
+                var length = line.Length;
+                var marker = line.Substring(0, 4);
+                window=marker.ToHashSet();
+                for (int i=4; i<length; i++)
+                {
+                    if(window.Count<4)
+                    {
+                        window.Add()
+                    }
+                }
+            }
+        }
+
+        public void Excute()
+        {
+            Console.WriteLine(elvesCal.OrderByDescending(x=>x).Take(3).Sum());
+        }
+
+    }
+}
